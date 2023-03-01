0bc6e3cb8a508b8c48618fdb8841506e9f65c223
Magnus Arnild Ipsen <102797036+kompasbank-magnus@users.noreply.github.com>
2023-03-01 15:30:26 +0100

    remove joint limit h1

--- simulate/controller.py
+++ simulate/controller.py
@@ -4,34 +4,36 @@
 from math import degrees
 
 STATE = None
-WALKSTEP = 0
-TRANSITIONSTEP = 0
-RULE = 2
 
-def get_angle_degrees(joint_positions):
-    return [degrees(pos) for pos in joint_positions]
+# SENSOR THRESHOLDS
+GROUND_THRESHOLD = 0.01
+OBSTACLE_THRESHOLD = 0.15
 
-def get_all_sensors_angle():
+# convert scalar to degrees
+def deg(scalar):
+    return round(degrees(scalar), 2)
+
+def get_sensor_values():
     global STATE
-    sensors =  [get_angle_degrees(STATE.sensor('a_h1').data), 
-                get_angle_degrees(STATE.sensor('b_h1').data),
-                get_angle_degrees(STATE.sensor('a_h2').data),
-                get_angle_degrees(STATE.sensor('b_h2').data),
-                get_angle_degrees(STATE.sensor('a_h3').data),
-                get_angle_degrees(STATE.sensor('b_h3').data), 
+    sensors =  [STATE.sensor('a_h1').data, 
+                STATE.sensor('b_h1').data,
+                STATE.sensor('a_h2').data,
+                STATE.sensor('b_h2').data,
+                STATE.sensor('a_h3').data,
+                STATE.sensor('b_h3').data, 
                 STATE.sensor('a_rangefinder_forward').data, 
                 STATE.sensor('b_rangefinder_forward').data,
                 STATE.sensor('a_rangefinder_down').data,
                 STATE.sensor('b_rangefinder_down').data]
-    # round all values to 5 decimal places and flatmap the list
+    # round all values to 4 decimal places and flatmap the list
     return [round(item, 4) for sublist in sensors for item in sublist]
 
 def is_grounded(module):
-    global STATE
+    global STATE, GROUND_THRESHOLD
     if (module == "a"):
-        return STATE.sensor("a_rangefinder_down").data < 0.01
+        return STATE.sensor("a_rangefinder_down").data < GROUND_THRESHOLD
     elif (module == "b"):
-        return STATE.sensor("b_rangefinder_down").data < 0.01
+        return STATE.sensor("b_rangefinder_down").data < GROUND_THRESHOLD
     else:
         print(is_grounded.__name__ + ": Invalid module name")
 
@@ -53,78 +55,25 @@ def set_rotate(module, ctrl):
     else:
         print(set_rotate.__name__ + ": Invalid module name")
 
-# Align the modules so that both the a_h1 and b_h1 joints are at 0 degrees
-# If the modules are not aligned, the module in front should rotate until they are
-def align_modules():
-    sensors = get_all_sensors_angle()
-    if (sensors[0] > 0):
-        set_rotate("a", -.1)
-    elif (sensors[0] < 0):
-        set_rotate("a", .1)
+def release_module(module):
+    global STATE
+    if (module == "a"):
+        STATE.actuator("a_thrust").ctrl = .01
+    elif (module == "b"):
+        STATE.actuator("a_thrust").ctrl = .01
     else:
-        set_rotate("a", 0)
+        print(release_module.__name__ + ": Invalid module name")
 
-
-def base_position():
-    sensors = get_all_sensors_angle()
-    set_rotate("b", 0)
-    set_thrust("a", -1)
-    set_thrust("b", .07)
-    if (sensors[0] > 0):
-        set_rotate("a", -.1)
-    elif (sensors[0] < 0):
-        set_rotate("a", .1)
+def is_obstructed(module):
+    global STATE, OBSTACLE_THRESHOLD
+    if (module == "a"):
+        return STATE.sensor("a_rangefinder_forward").data < OBSTACLE_THRESHOLD
+    elif (module == "b"):
+        return STATE.sensor("b_rangefinder_forward").data < OBSTACLE_THRESHOLD
     else:
-        set_rotate("a", 0)
-        set_thrust("b", -1)
+        print(is_obstructed.__name__ + ": Invalid module name")
 
-def walk():
-    global WALKSTEP
-    sensors = get_all_sensors_angle()
-    print(sensors[0], sensors[1])
-    if (sensors[6] < 0.15 and sensors[7] < 0.15):
-        set_thrust("a", -1)
-        set_thrust("b", -1)
-        return
-    if (WALKSTEP == 0):
-        if (sensors[6] < 0.15):
-            return
-        set_thrust("a", -1)
-        set_thrust("b", .07)
-        angle = sensors[0]
-        if (angle > -30):
-            set_rotate("a", -.3)
-        else:
-            set_rotate("a", 0)
-            WALKSTEP = 1
-    if (WALKSTEP == 1):
-        if (sensors[7] < 0.15):
-            return
-        set_thrust("a", .07)
-        set_thrust("b", -1)
-        angle = sensors[1]
-        if (angle < 30):
-            set_rotate("b", .3)
-        else:
-            set_rotate("b", 0)
-            WALKSTEP = 0
-
-def transition():
-    global STATE, TRANSITIONSTEP
-    sensors = get_all_sensors_angle()
-
-    if (TRANSITIONSTEP == 0):
-        base_position()
-        if (sensors[0] < .5 or sensors[1] > -.5):
-            TRANSITIONSTEP = 1
-    elif (TRANSITIONSTEP == 1):
-        set_rotate("a", -.15)
-        set_rotate("b", 0)
-        set_thrust("a", -1.)
-        set_thrust("b", .2)
-        set_thrust("b", .075)
-
-def print_state():
+def print_actuators():
     global STATE
     a_rotate = STATE.actuator("a_rotor").ctrl
     b_rotate = STATE.actuator("b_rotor").ctrl
@@ -132,38 +81,28 @@ def print_state():
     b_thrust = STATE.actuator("b_thrust").ctrl
     print("a_rotate: ", a_rotate, "b_rotate: ", b_rotate, "a_thrust: ", a_thrust, "b_thrust: ", b_thrust)
 
+def print_sensors():
+    sensors = get_sensor_values()
+    print("a_h1: ", sensors[0], "b_h1: ", sensors[1], 
+          "\na_h2: ", sensors[2], "b_h2: ", sensors[3], 
+          "\na_h3: ", sensors[4], "b_h3: ", sensors[5], 
+          "\na_rangefinder_forward: ", sensors[6], "b_rangefinder_forward: ", sensors[7], 
+          "\na_rangefinder_down: ", sensors[8], "b_rangefinder_down: ", sensors[9])
 
 def controller(model, data):
-    global STATE, RULE, WALKSTEP, TRANSITIONSTEP
-
-    b_h1 = data.joint('b_h1')
-    b_h1.qpos = 0
-    b_h1.qvel = 0
-    b_h1.qacc = 0
-
-    
-    
-
-    
+    global STATE
     STATE = data
-    sensors = get_all_sensors_angle()
-        
-    if is_grounded("a") and is_grounded("b"):
-        if (RULE == 0):
-            walk()
-            #print(sensors[6], sensors[7])
-        elif (RULE == 1):
-            transition()
-        elif (RULE == 2):
-            #base_position()
-            set_thrust("a", -1)
-            set_thrust("b", .07)
-            angle = sensors[0]
-            if (angle > -30):
-                set_rotate("a", -.3)
-            else:
-                set_rotate("a", 0)
-                align_modules()
-    print(sensors[0], sensors[1])
+    print(deg(data.sensor('a_h1').data), deg(data.sensor('b_h1').data))
+    if (is_grounded("a") and is_grounded("b")):
+        set_thrust("a", -1)
+        set_thrust("b", 0)
+        a_angle = get_sensor_values()[0]
+        if (deg(a_angle) > -90.):
+            set_rotate("a", -.75)
+        else:
+            set_rotate("a", 0)
+            set_thrust("b", -1)
+            set_thrust("a", 0)
+
 
     pass
